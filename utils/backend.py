a702b1462b032a3ae7c5797df07174b45a5e515c
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-12-01 21:54:09 +0300

    beautifid

--- utils/backend.py
+++ utils/backend.py
@@ -1,11 +1,10 @@
-from typing import DefaultDict, NamedTuple
 import logging
 
 from epicgamesnotification.commons._save import _save
 from epicgamesnotification.utils.utils import open_file
 from epicgamesnotification.apps.scrape.tasks import Scraper
 
-from utils.utils import BackendState, Retailers, SaveOrNot, SendMail
+from utils.utils import BackendState, SaveOrNot
 
 scraper = Scraper()
 
@@ -14,7 +13,6 @@ def backend(state: BackendState):
     # collect vars
     # print(state)
     # print(state.retailer_id, state.save_or_not, state.send_mail)
-
     """
     Scrape ederken backend tarafina backend tarafina;
         • retailer_id
@@ -22,14 +20,13 @@ def backend(state: BackendState):
         • send_mail
 
     """
-    print('raw state: ', state, list(SaveOrNot))
+    print("raw state: ", state, list(SaveOrNot))
 
     _state = {
         # 'save': True if list(SaveOrNot)[state.save_or_not].value in (0, 1) else False,
-        'open': True if list(SaveOrNot)[state.save_or_not].value in (0, 2) else False,
-        'send_mail': True 
+        "open": True if list(SaveOrNot)[state.save_or_not].value in (0, 2) else False,
+        "send_mail": True,
     }
-
 
     # scrape data
     # TODO: scrape kismi su an icin yalnizca epic icin tanimli oldugundan dolayi
@@ -41,13 +38,14 @@ def backend(state: BackendState):
         # after scrape data if save?
         # if _state.get('save'):
         is_saved, saved_path_or_error = _save._save(data)
-        print('saved?: ', is_saved, saved_path_or_error)
-        if _state.get('open'):
-            open_file(saved_path_or_error)
-        if _state.get('send_mail'):
-            print('mail sent')
-
+        print("saved?: ", is_saved, saved_path_or_error)
+        if _state.get("open"):
+            open_file(path=saved_path_or_error)
+        if _state.get("send_mail"):
+            print("mail sent")
 
     except Exception as e:
-        pass
-
+        logging.error(
+            "Error occured while scraping/saving data or sending email", exc_info=e
+        )
+        raise e
